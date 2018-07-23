00f3c610c0b5701cc71d36880a92523a16195588
kk87lk <786279458@qq.com>
2018-07-23 13:09:21 +0800

    2

--- Basic-operation/src/Import.java
+++ Basic-operation/src/Import.java
@@ -6,12 +6,18 @@ public class Import {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner in = new Scanner(System.in);
-		System.out.println(in.nextLine());
-		int n1 = in.nextInt();
-		if (n1 == 1)
-			System.out.println(n1 + 2);
-		else
-			System.out.println(n1);
+		int i = 0;
+		int[] numbers = new int[100];
+		int record = in.nextInt();
+		while (record != 0){
+			numbers[i++] = record;
+			record = in.nextInt();
+		}
+		int n = 0;
+		System.out.println(numbers[4]);
+		while (numbers[n] != 0) {
+			System.out.println(numbers[n++]);
+		}
 		in.close();
 	}
 	
