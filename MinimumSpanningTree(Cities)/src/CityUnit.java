adb9f474146888ce3a2b275f6664ee8a4128a6b9
kk87lk <786279458@qq.com>
2018-11-18 14:15:37 +0800

    1

---
+++ MinimumSpanningTree(Cities)/src/CityUnit.java
@@ -0,0 +1,5 @@
+
+public class CityUnit {
+    private String cityName;
+    private 
+}
---
+++ MinimumSpanningTree(Cities)/src/Mydraw.java
@@ -0,0 +1,7 @@
+import javax.swing.JPanel;
+
+public class Mydraw {
+
+    public void drawCityUnit(JPanel panel, int x, int y) {
+    }
+}
